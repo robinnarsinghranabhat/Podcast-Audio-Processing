0ddfdf3e89f134831b57b9007c11c45ce58edef3
unknown <robinnarsingha123@gmail.com>
2021-03-13 19:47:54 +0545

    First draft of Successful Traning

--- src/model.py
+++ src/model.py
@@ -3,50 +3,97 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+
+#         self.drp = nn.Dropout2d(0.1)
+#         self.pool1 = nn.MaxPool2d(2, stride=1)
+#         self.pool2 = nn.MaxPool2d(2, stride=2)
+#         self.pool3 = nn.MaxPool2d(2, stride=2)
+
+#         self.conv1 = nn.Conv2d(1, 16, 3, dilation=1)  ## inchannel , outchannel , kernel size ..
+#         self.conv1_bn = nn.BatchNorm2d(16)
+
+#         self.conv2 = nn.Conv2d(16, 32, 5, dilation=2)
+#         self.conv2_bn = nn.BatchNorm2d(32)
+
+#         self.conv3 = nn.Conv2d(32, 64, 5, dilation=4)
+#         self.conv3_bn = nn.BatchNorm2d(64)
+
+#         # self.conv4 = nn.Conv2d(64, 64, 7, dilation=8)
+#         # self.conv4_bn = nn.BatchNorm2d(64)
+
+#         self.fc1 = nn.Linear(64 * 21 * 161, 32)
+#         self.fc2 = nn.Linear(32, 1)
+
+#     def forward(self, x):
+#         x = self.conv1_bn(self.pool1(F.relu(self.conv1(x))))
+#         x = self.drp(x)
+
+#         x = self.conv2_bn(self.pool2(F.relu(self.conv2(x))))
+#         x = self.drp(x)
+
+#         x = self.conv3_bn(self.pool2(F.relu(self.conv3(x))))
+#         x = self.drp(x)
+
+#         # x = self.conv4_bn(self.pool3(F.relu(self.conv4(x))))
+#         # x = self.drp(x)
+#         # x = self.drp(self.pool1(F.relu(self.conv4(x))))
+#         # x = self.drp(self.pool2(F.relu(self.conv5(x))))
+#         # size = torch.flatten(x).shape[0]
+#         x = x.view(-1, 64 * 21 * 161)
+#         # x = x.unsqueeze_(1)
+#         # print(x.shape)
+#         x = F.relu(self.fc1(x))
+#         x = self.fc2(x)
+#         return torch.sigmoid(x)
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.drp = nn.Dropout2d(0.1)
+        self.drp = nn.Dropout2d(0.2)
+        self.fc_drp = nn.Dropout(0.2)
         self.pool1 = nn.MaxPool2d(2, stride=1)
         self.pool2 = nn.MaxPool2d(2, stride=2)
-        self.pool3 = nn.MaxPool2d(2, stride=2)
+        self.pool3 = nn.MaxPool2d(2, stride=3)
 
-        self.conv1 = nn.Conv2d(1, 16, 3, dilation=1)  ## inchannel , outchannel , kernel size ..
-        self.conv1_bn = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(1, 24, 5, dilation=1, stride=1)   # inchannel , outchannel , kernel size ..
+        self.conv1_bn = nn.BatchNorm2d(24)
 
-        self.conv2 = nn.Conv2d(16, 32, 5, dilation=2)
+        self.conv2 = nn.Conv2d(24, 32, 7, dilation=1, stride=1)
         self.conv2_bn = nn.BatchNorm2d(32)
 
-        self.conv3 = nn.Conv2d(32, 64, 5, dilation=4)
+        self.conv3 = nn.Conv2d(32, 64, 9, dilation=1, stride=2)
         self.conv3_bn = nn.BatchNorm2d(64)
 
-        # self.conv4 = nn.Conv2d(64, 64, 7, dilation=8)
-        # self.conv4_bn = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, 11, dilation=1, stride=2)
+        self.conv4_bn = nn.BatchNorm2d(64)
 
-        self.fc1 = nn.Linear(64 * 21 * 161, 32)
-        self.fc2 = nn.Linear(32, 1)
+        self.fc1 = nn.Linear(64*3*26, 64)
+        self.fc2 = nn.Linear(64, 1)
 
     def forward(self, x):
         x = self.conv1_bn(self.pool1(F.relu(self.conv1(x))))
         x = self.drp(x)
 
-        x = self.conv2_bn(self.pool2(F.relu(self.conv2(x))))
+        x = self.conv2_bn(self.pool1(F.relu(self.conv2(x))))
         x = self.drp(x)
 
         x = self.conv3_bn(self.pool2(F.relu(self.conv3(x))))
         x = self.drp(x)
 
-        # x = self.conv4_bn(self.pool3(F.relu(self.conv4(x))))
-        # x = self.drp(x)
-        # x = self.drp(self.pool1(F.relu(self.conv4(x))))
-        # x = self.drp(self.pool2(F.relu(self.conv5(x))))
-        # size = torch.flatten(x).shape[0]
-        x = x.view(-1, 64 * 21 * 161)
-        # x = x.unsqueeze_(1)
-        # print(x.shape)
+        x = self.conv4_bn(self.pool3(F.relu(self.conv4(x))))
+        x = self.drp(x)
+
+        # import pdb ; pdb.set_trace()
+        x = x.view(-1, 64*3*26)
         x = F.relu(self.fc1(x))
+        x = self.fc_drp(x)
+
         x = self.fc2(x)
+
         return torch.sigmoid(x)
 
 
